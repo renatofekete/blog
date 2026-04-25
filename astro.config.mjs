d9d6941b65f227b07de1e22fdc013774d693a00d
Renato <renatofekete.91@gmail.com>
2026-04-25 13:28:18 +0200

    added change to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,30 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-   vite: {
+  vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
     drafts: true,
     shikiConfig: {
-      theme: "css-variables"
-    }
+      theme: "css-variables",
+    },
   },
   shikiConfig: {
     wrap: true,
     skipInline: false,
-    drafts: true
+    drafts: true,
   },
-  site: 'https://yourwebsite.com',
-  integrations: [sitemap(), mdx()]
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
+    },
+  },
+  site: "https://yourwebsite.com",
+  integrations: [sitemap(), mdx()],
 });
